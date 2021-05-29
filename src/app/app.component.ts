9a23d8b964f4fc0d1de4ebd25be6e946d4eb00eb
NMehdiT <nabyl.mehdi.tahenni@gmail.com>
2021-05-29 15:40:58 +0200

    Terminer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
+  public pseudo = "NMehdiT";
+  public typeInput = "date";
+
+  public sauvegarde() {
+    console.log("Clic");
+  }
 }
